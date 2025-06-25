2d11da52078a4864a07bfd89e556fcdad6d73116
Anuroop <141569865+ZBREFLEX@users.noreply.github.com>
2025-06-25 12:32:19 +0530

    Updated db.js

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12786354",
-  password: "6By9nsFXJU", // default XAMPP password
-  database: "sql12786354",
+  host: "localhost",
+  user: "root",
+  password: "", // default XAMPP password
+  database: "drug_monitor",
 });
 
 module.exports = db;
